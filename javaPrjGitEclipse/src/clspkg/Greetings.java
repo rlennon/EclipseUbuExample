c13ef4103f30925abd747d506bcc736405e8152f
Ruth Lennon <Ruth.Lennon@lyit.ie>
2020-01-28 14:57:57 +0000

    A goodbye message was added.

--- javaPrjGitEclipse/src/clspkg/Greetings.java
+++ javaPrjGitEclipse/src/clspkg/Greetings.java
@@ -15,6 +15,7 @@ public class Greetings {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Greetings LYIT student");
+		System.out.println("Goodbye LYIT student");
 	}
 
 }
