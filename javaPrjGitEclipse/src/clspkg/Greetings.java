23812dcbac3ce1f450866deaeaa36eb285b43b71
Ruth Lennon <Ruth.Lennon@lyit.ie>
2020-01-28 14:20:02 +0000

    A change was made to Greetings.java

---
+++ javaPrjGitEclipse/src/clspkg/Greetings.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package clspkg;
+
+/**
+ * @author l0012345
+ *
+ */
+public class Greetings {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Greetings LYIT student");
+	}
+
+}
